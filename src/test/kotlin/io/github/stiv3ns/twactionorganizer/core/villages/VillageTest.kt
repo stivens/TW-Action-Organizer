de561f8d9b9d69d924249d9baab845045cbbf1a8
stivens <jacekbizub@gmail.com>
2026-02-16 17:19:19 +0100

    upgrade versions

--- src/test/kotlin/io/github/stiv3ns/twactionorganizer/core/villages/VillageTest.kt
+++ src/test/kotlin/io/github/stiv3ns/twactionorganizer/core/villages/VillageTest.kt
@@ -1,8 +1,6 @@
 package io.github.stiv3ns.twactionorganizer.core.villages
 
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.data.forAll
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class VillageTest : WordSpec({
@@ -15,31 +13,17 @@ class VillageTest : WordSpec({
 
     "Village.distance" should {
         "return proper squared distance between given villages" {
-            forAll(
-                row(
-                    StubVillageFactory.getVillage(500, 500),
-                    StubVillageFactory.getVillage(500, 501),
-                    1
-                ),
-                row(
-                    StubVillageFactory.getVillage(500, 500),
-                    StubVillageFactory.getVillage(500, 510),
-                    100
-                ),
-                row(
-                    StubVillageFactory.getVillage(500, 500),
-                    StubVillageFactory.getVillage(523, 614),
-                    13525
-                ),
-                row(
-                    StubVillageFactory.getVillage(318, 219),
-                    StubVillageFactory.getVillage(411, 200),
-                    9010
-                ),
-            ) { v1, v2, squaredDist ->
-                v1 distanceTo v2 shouldBe squaredDist
-            }
+            StubVillageFactory.getVillage(500, 500) distanceTo
+                StubVillageFactory.getVillage(500, 501) shouldBe 1
+
+            StubVillageFactory.getVillage(500, 500) distanceTo
+                StubVillageFactory.getVillage(500, 510) shouldBe 100
+
+            StubVillageFactory.getVillage(500, 500) distanceTo
+                StubVillageFactory.getVillage(523, 614) shouldBe 13525
+
+            StubVillageFactory.getVillage(318, 219) distanceTo
+                StubVillageFactory.getVillage(411, 200) shouldBe 9010
         }
     }
 })
-
